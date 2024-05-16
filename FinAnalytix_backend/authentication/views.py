3b9fdb484fb38b2aaa7915bb7162fce917bb56ba
Jemimah Mmboga <mmbogajemimah@gmail.com>
2024-05-16 19:33:08 +0300

    Login View Function

--- FinAnalytix_backend/authentication/views.py
+++ FinAnalytix_backend/authentication/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
+from django.contrib.auth import authenticate, login
 
 from .serializers import UserRegistrationSerializer
 from .forms import NewUserForm
@@ -48,3 +49,31 @@ def register_user(request):
      
     # Render the registration page template (GET request)
     return render(request, 'register.html')
+
+#Define a View Function that Logs In a User
+def login_user(request):
+    #Check if the HTTP request method is POST (Form Submission)
+    if request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        
+        #Check if user with the provided username exists
+        if not User.objects.filter(username=username).exists():
+            # Display Error message if the username does not exist
+            messages.error(request, "Invalid Username")
+            return redirect('/login/')
+    
+        # Authenticate the user with the provided username and password
+        user = authenticate(username=username, password=password)
+        
+        if user is None:
+            # Display Error message if the password is incorrect
+            messages.error(request, "Invalid Password")
+            return redirect('/login/')
+        else:
+            # Log the user in and redirect them to the home page
+            login(request, user)
+            return redirect('/home/')
+    # Render the login page template (GET request)
+    return render(request, 'login.html')
